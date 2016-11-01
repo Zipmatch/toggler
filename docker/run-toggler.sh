3a8cb9d219781cf1a1af032bdfcea5d78bb37c52
Oliver Godby <oliver_godby@me.com>
2016-11-01 18:11:18 +0800

    forgot to update shell script

--- docker/run-toggler.sh
+++ docker/run-toggler.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xmx2048m -Xms512m -jar /code/toggler-1.1.4-standalone.jar /code/toggler-configs/
+java -Xmx2048m -Xms512m -jar /code/toggler-1.1.5-standalone.jar /code/toggler-configs/
